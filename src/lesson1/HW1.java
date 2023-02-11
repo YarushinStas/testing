7e29fef87ab1d366204fefaabd4c7fa6c611bbb5
Alex <kovalev.alexxtas.17@gmail.com>
2023-02-11 20:08:04 +0300

    Выполнил первое ДЗ

---
+++ src/lesson1/HW1.java
@@ -0,0 +1,10 @@
+package lesson1;
+
+public class HW1 {
+    public static void main(String[] args) {
+
+    }
+    private static void task1(){
+        System.out.println("Решение первой задачи");
+    }
+}
